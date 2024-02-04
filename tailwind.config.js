2c869f8b4ec28707fa9126b62c28cad064c0590d
mahdi hosseini <hosseinim018@gmail.com>
2024-02-04 14:05:42 +0330

    add my font to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}', "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         mono: ['Fira Code', 'monospace'],
+        shabnam: ['Shabnam']
       },
     },
   },
