35f22b27b2e998917b766476d724a18d4b84a8f8
mahdi hosseini <hosseinim018@gmail.com>
2024-02-04 15:15:18 +0330

    h-screen height of sidebar and fix height of each base of sidebar

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,21 @@ export default {
 <template>
   <div class="grid grid-cols-12 h-screen">
 <!--    sidebar-->
-    <div class="col-span-2 bg-gray-100">
-
+    <div class="col-span-2 bg-gray-100 h-screen">
+      <div class="flex-row h-screen">
+<!--        header of side-bar-->
+        <div class="grid grid-cols-6 bg-green-200 h-[25%]">
+          s
+<!--&lt;!&ndash;          account picture and email&ndash;&gt;-->
+<!--          <div class="col-span-5 bg-red-400 p-10 h"></div>-->
+<!--&lt;!&ndash;          switch dark mode&ndash;&gt;-->
+<!--          <div class="col-span-1 bg-yellow-400 p-10"></div>-->
+        </div>
+<!--        content of side-bar-->
+        <div class="bg-red-200 h-[60%]">content</div>
+<!--        footer of side-bar-->
+        <div class="bg-blue-200 h-[15%]">footer</div>
+      </div>
     </div>
 <!--    main content-->
     <div class="col-span-10 bg-gray-300 p-6"></div>
