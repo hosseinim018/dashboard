61ca94e6f2b3c75a968ec811997e84a68faac714
mahdi hosseini <hosseinim018@gmail.com>
2024-02-04 23:33:13 +0330

    added navbar to main content of page

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 <!--    sidebar-->
     <div class="col-span-2 h-screen">
       <aside class="grid grid-rows-8 h-screen">
-        <header class="row-span-1 bg-blue-500 shadow md:shadow-lg">
+        <header class="row-span-1 bg-blue-500">
           <div class="flex h-full">
             <div class="flex items-center justify-between">
 <!--              avatar with text-->
@@ -93,8 +93,22 @@ export default {
       </aside>
     </div>
 <!--    main content-->
-    <div class="col-span-9 bg-gray-200 p-6">
-      <div class="container h-100">
+    <div class="col-span-9 bg-gray-200">
+      <nav class="w-100 h-20 bg-blue-500 flex items-center px-6 border-red-100">
+        <form class="w-1/3">
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+                <button type="submit" class="text-slate-200 absolute end-2.5 bottom-2.5 bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+        </form>
+      </nav>
+      <div class="px-6 h-100">
         <Home/>
       </div>
     </div>
