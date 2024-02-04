22128dcec9b7259cf158777000758cf13eb9acdf
mahdi hosseini <hosseinim018@gmail.com>
2024-02-04 21:11:56 +0330

    rewrite sibar and add list qroup to main of sidebar

--- src/App.vue
+++ src/App.vue
@@ -10,33 +10,45 @@ export default {
 
 
 <template>
-  <div class="grid grid-cols-12 h-screen">
+  <div class="grid grid-cols-11 h-screen">
 <!--    sidebar-->
     <div class="col-span-2 bg-gray-100 h-screen">
-      <div class="flex-row h-screen">
-<!--        header of side-bar-->
-        <div class="grid grid-cols-6 bg-green-200 h-[25%]">
-<!--          account picture and email-->
-          <div class="col-span-5 bg-red-400">
-            <div class="bg-blue-600 h-[70%] w-full">
-              <div class="w-20 h-20 p-5 bg-gray-300"></div>
+      <aside class="grid grid-rows-8 bg-red-200 h-screen">
+        <header class="row-span-1 bg-green-500">
+          <div class="flex h-full">
+            <div class="flex items-center justify-between bg-red-500">
+<!--              avatar with text-->
+              <div class="flex items-center gap-2 bg-blue-600">
+                <div class="hidden relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                    <span class="font-medium text-gray-600 dark:text-gray-300">MH</span>
+                </div>
+                <img class="w-10 h-10 rounded-full ring-2 ring-gray-300" src="./assets/avatar1.avif" alt="Rounded avatar">
+                <div class="font-medium dark:text-white">
+                    <h5 class="text-lg font-medium">Mahdi.H</h5>
+                    <div class="text-sm">hosseinim018@gmail.com</div>
+                </div>
+              </div>
             </div>
-            <div class="bg-blue-200 h-[30%] w-full">
-              <h>Mahdi.H</h>
-              <h1>hosseinim018@gmail.com</h1>
-            </div>
+            <div class="bg-gray-100 mx-auto">002</div>
           </div>
-<!--          switch dark mode-->
-          <div class="col-span-1 bg-yellow-400 p-10"></div>
-        </div>
-<!--        content of side-bar-->
-        <div class="bg-red-200 h-[60%]">content</div>
-<!--        footer of side-bar-->
-        <div class="bg-blue-200 h-[15%]">footer</div>
-      </div>
+        </header>
+        <main class="row-span-6 bg-yellow-200">
+          <ul class="w-100 text-sm font-medium text-gray-900 bg-white m-3">
+              <li class="w-full px-4 py-2 cursor-pointer hover:bg-gray-100 hover:text-blue-700 relative inline-flex items-center">
+                <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+                </svg>
+                Downlaod
+              </li>
+          </ul>
+        </main>
+        <footer class="row-span-1 bg-blue-600">footer</footer>
+      </aside>
     </div>
 <!--    main content-->
-    <div class="col-span-10 bg-gray-300 p-6"></div>
+    <div class="col-span-9 bg-gray-300 p-6">
+
+    </div>
     
   </div>
 </template>
