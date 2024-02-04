f88476584a355098513fbadd0bb7d224268dcbce
mahdi hosseini <hosseinim018@gmail.com>
2024-02-04 14:22:38 +0330

    comment defult style app in style tag

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,19 @@ export default {
 <template>
   <div class="grid grid-cols-12">
 <!--    sidebar-->
-    <div class="col-span-3 bg-gray-100"></div>
-    <div class="col-span-9 bg-gray-300 p-6"></div>
+    <div class="col-span-2 bg-gray-100">
+
+    </div>
 <!--    main content-->
+    <div class="col-span-10 bg-gray-300 p-6"></div>
   </div>
 </template>
 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*}*/
 </style>
