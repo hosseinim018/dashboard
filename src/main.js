228dc6bfe9929b99fe8ef0b9e3fc32ac64b436bb
mahdi hosseini <hosseinim018@gmail.com>
2024-02-08 22:15:28 +0330

    install and config app with mitt

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
-
-createApp(App).mount('#app')
+import 'flowbite';
+import VueApexCharts from "vue3-apexcharts";
+import { emitter } from "@/emitter";
+const app = createApp(App)
+app.config.globalProperties.emitter = emitter;
+app.use(VueApexCharts);
+app.mount('#app')
+// createApp(App).mount('#app')
